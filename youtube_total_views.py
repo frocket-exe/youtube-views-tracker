6d6bc0875e976eb28b83038605431c2a0f01e1e3
frocket-exe <freddie.rockett@gmail.com>
2026-03-17 21:59:56 +0000

    Add subject to email

--- youtube_total_views.py
+++ youtube_total_views.py
@@ -135,8 +135,11 @@ with open("milestones.json") as f:
 def email(achieved, milestone, time):
     if achieved:
         emailBody = (f"\nYou achieved {milestone:,} views on {time}\n")
+        emailSubject = (f"Achieved milestone - {milestone:,}")
     else:
         emailBody = (f"\nYou will achieve {milestone:,} views in {time} minutes\n")
+        emailSubject = (f"Upcoming milestone - {milestone:,}")
+    print(emailSubject)
     print(emailBody)
 
 def milestoneDate(milestone):
