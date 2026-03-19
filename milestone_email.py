5d1014c7f7790627dacea092083d695c9b9a4041
frocket-exe <freddie.rockett@gmail.com>
2026-03-19 15:04:58 +0000

    Update milestone_email.py

--- milestone_email.py
+++ milestone_email.py
@@ -1,9 +1,19 @@
-def send_email(achieved, milestone, time):
+def email(achieved, milestone, time):
+    senderEmail = os.getenv("sendEmail")
+    loginEmail = os.getenv("loginEmail")
+    senderPassword = os.getenv("senderPW")
+    receiverEmail = os.getenv("receiveEmail")
+
     if achieved:
         emailBody = (f"\nYou achieved {milestone:,} views on {time}\n")
         emailSubject = (f"Achieved milestone - {milestone:,}")
     else:
         emailBody = (f"\nYou will achieve {milestone:,} views in {time} minutes\n")
         emailSubject = (f"Upcoming milestone - {milestone:,}")
-    print(emailSubject)
-    print(emailBody)
+        
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(loginEmail, senderPassword)
+    message = (f"Subject: {emailSubject}\n\n{emailBody}")
+    s.sendmail(senderEmail, receiverEmail, message)
+    s.quit()
