26eeec50449e754fc36307d8712926fa9909026e
frocket-exe <freddie.rockett@gmail.com>
2026-04-24 09:39:50 +0100

    Change emails function to take subject and body as parameters

--- milestone_email.py
+++ milestone_email.py
@@ -1,18 +1,11 @@
 import os
 import smtplib
 
-def email(achieved, milestone, time):
+def email(emailSubject, emailBody):
     senderEmail = os.getenv("SEND_EMAIL")
     loginEmail = os.getenv("LOGIN_EMAIL")
     senderPassword = os.getenv("SEND_PW")
     receiverEmail = os.getenv("RECEIVE_EMAIL")
-
-    if achieved:
-        emailBody = (f"\nYou achieved {milestone:,} views on {time}\n")
-        emailSubject = (f"Achieved milestone - {milestone:,}")
-    else:
-        emailBody = (f"\nYou will achieve {milestone:,} views in {time} minutes\n")
-        emailSubject = (f"Upcoming milestone - {milestone:,}")
         
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
